ab9bd8896984195e5f882de036fb0d8fdfecb34c
risalshahed <36281118+risalshahed@users.noreply.github.com>
2018-03-14 03:17:21 +0600

    Update Lexical Scopping.R

--- Lexical Scopping.R
+++ Lexical Scopping.R
@@ -27,6 +27,23 @@ cachemean <- function(x, ...) {
 }
 
 ##Cache Matrix
+
+## Functions that cache the inverse of a matrix
+
+## source('cachematrix.R')
+## m <- makeCacheMatrix(matrix(1:4, nrow=2, ncol=2)))
+## cacheSolve(m)
+##       [,1] [,2]
+## [1,]   -2  1.5
+## [2,]   1  -0.5
+
+## Let us create a special "matrix" that is a list containing
+## a function to
+##   * set the value of the matrix
+##   * get the value of the matrix
+##   * set the value of the inverse matrix
+##   * get the value of the inverse matrix
+
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   set <- function(y) {
@@ -43,6 +60,13 @@ makeCacheMatrix <- function(x = matrix()) {
     getinverse = getinverse
   )
 }
+
+##  Solve the inverse of the cached square matrix created by
+##  makeCacheMatrix above. If the inverse of the cached matrix
+##  has already been solved, return the cached version of the
+##  inverse matrix. Otherwise, solve the inverse matrix and
+##  store it in the cache.
+
 
 cacheSolve <- function(x, ...) {
   i <- x$getinverse()
